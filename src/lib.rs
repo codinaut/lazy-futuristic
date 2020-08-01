d7328b63d4b8beb9d6fa22e1a2e9bab8f46813a5
Daniel <codinaut@gmail.com>
2020-08-01 23:30:01 +0700

    feat: get or set value

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,47 @@
+use std::cell::UnsafeCell;
+
 #[derive(Debug)]
-pub enum ValueOrSetter<'v, T> {
-    Value(&'v T),
-    Setter(LazySetter),
+pub enum ValueOrSetter<'l, T> {
+    Value(&'l T),
+    Setter(LazySetter<'l, T>),
 }
 
 #[derive(Debug)]
-pub struct LazySetter {}
+pub struct LazySetter<'l, T> {
+    parent: &'l Lazy<T>
+}
 
+impl<'l, T> LazySetter<'l, T> {
+    pub fn set(&self, value: T) -> &'l T {
+        unsafe {
+            *self.parent.value.get() = Some(value);
+        };
+        self.parent.get().unwrap()
+    }
+}
+
+#[derive(Debug)]
 pub struct Lazy<T> {
-    value: Option<T>,
+    value: UnsafeCell<Option<T>>,
 }
 
 impl<T> Lazy<T> {
     pub fn new() -> Self {
-        Self { value: None }
+        Self { value: UnsafeCell::new(None) }
     }
 
     pub fn get_or_set(&self) -> ValueOrSetter<T> {
-        match &self.value {
-            Some(_) => todo!(),
-            None => ValueOrSetter::Setter(LazySetter {}),
+        match unsafe { self.value.get().as_ref().unwrap() } {
+            Some(value) => ValueOrSetter::Value(value),
+            None => ValueOrSetter::Setter(LazySetter::<T> { parent: &self })
         }
     }
 
     pub fn get(&self) -> Option<&T> {
-        None
+        match self.get_or_set() {
+            ValueOrSetter::Value(value) => Some(value),
+            ValueOrSetter::Setter(_) => None
+        }
     }
 }
 
@@ -47,5 +64,17 @@ mod tests {
     fn get_or_set() {
         let lazy = Lazy::<i32>::new();
         assert!(matches!(lazy.get_or_set(), ValueOrSetter::Setter(_)));
+    }
+
+    #[test]
+    fn get_or_set_value() {
+        let lazy: Lazy<i32> = Lazy::new();
+        assert_eq!(
+            *match lazy.get_or_set() {
+                ValueOrSetter::Value(value) => value,
+                ValueOrSetter::Setter(setter) => setter.set(5),
+            },
+            5
+        )
     }
 }
