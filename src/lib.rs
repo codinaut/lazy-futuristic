6175438491a575f3bdc6d68ea900313e6681ed49
Daniel <codinaut@gmail.com>
2020-08-02 00:34:39 +0700

    feat: lock mechanism

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use std::cell::UnsafeCell;
+use std::sync::atomic::{AtomicBool, Ordering};
+use futures::lock::{Mutex, MutexGuard};
 
 #[derive(Debug)]
 pub enum ValueOrSetter<'l, T> {
@@ -8,39 +10,60 @@ pub enum ValueOrSetter<'l, T> {
 
 #[derive(Debug)]
 pub struct LazySetter<'l, T> {
-    parent: &'l Lazy<T>
+    parent: &'l Lazy<T>,
+    guard: MutexGuard<'l, ()>,
 }
 
 impl<'l, T> LazySetter<'l, T> {
     pub fn set(&self, value: T) -> &'l T {
         unsafe {
             *self.parent.value.get() = Some(value);
+            self.parent.is_initialized.store(true, Ordering::Release);
         };
-        self.parent.get().unwrap()
+        self.parent.extract()
     }
 }
 
 #[derive(Debug)]
 pub struct Lazy<T> {
+    is_initialized: AtomicBool,
+    lock: Mutex<()>,
     value: UnsafeCell<Option<T>>,
 }
 
 impl<T> Lazy<T> {
     pub fn new() -> Self {
-        Self { value: UnsafeCell::new(None) }
-    }
-
-    pub fn get_or_set(&self) -> ValueOrSetter<T> {
-        match unsafe { self.value.get().as_ref().unwrap() } {
-            Some(value) => ValueOrSetter::Value(value),
-            None => ValueOrSetter::Setter(LazySetter::<T> { parent: &self })
+        Self {
+            is_initialized: AtomicBool::new(false),
+            lock: Mutex::new(()),
+            value: UnsafeCell::new(None),
         }
     }
 
-    pub fn get(&self) -> Option<&T> {
-        match self.get_or_set() {
+    fn extract(&self) -> &T {
+        match unsafe { self.value.get().as_ref().unwrap() } {
+            Some(value) => value,
+            None => unreachable!(),
+        }
+    }
+
+    pub async fn get_or_set<'l>(&'l self) -> ValueOrSetter<'l, T> {
+        if self.is_initialized.load(Ordering::Relaxed) {
+            return ValueOrSetter::Value(self.extract())
+        }
+
+        let guard = self.lock.lock().await;
+        if self.is_initialized.load(Ordering::Relaxed) {
+            ValueOrSetter::Value(self.extract())
+        } else {
+            ValueOrSetter::Setter(LazySetter::<T> { parent: &self, guard })
+        }
+    }
+
+    pub async fn get(&self) -> Option<&T> {
+        match self.get_or_set().await {
             ValueOrSetter::Value(value) => Some(value),
-            ValueOrSetter::Setter(_) => None
+            ValueOrSetter::Setter(_) => None,
         }
     }
 }
@@ -54,27 +77,38 @@ mod tests {
         Lazy::<()>::new();
     }
 
-    #[test]
-    fn get_unset() {
-        let lazy = Lazy::<i32>::new();
-        assert_eq!(lazy.get(), None)
-    }
-
-    #[test]
-    fn get_or_set() {
-        let lazy = Lazy::<i32>::new();
-        assert!(matches!(lazy.get_or_set(), ValueOrSetter::Setter(_)));
-    }
-
-    #[test]
-    fn get_or_set_value() {
+    #[tokio::test]
+    async fn get_unset() {
         let lazy: Lazy<i32> = Lazy::new();
-        assert_eq!(
-            *match lazy.get_or_set() {
-                ValueOrSetter::Value(value) => value,
-                ValueOrSetter::Setter(setter) => setter.set(5),
-            },
-            5
-        )
+        assert_eq!(lazy.get().await, None)
+    }
+
+    #[tokio::test]
+    async fn get_or_set() {
+        let lazy: Lazy<i32> = Lazy::new();
+        assert!(matches!(lazy.get_or_set().await, ValueOrSetter::Setter(_)));
+    }
+
+    #[tokio::test]
+    async fn get_or_set_value() {
+        let lazy: Lazy<i32> = Lazy::new();
+        let value = match lazy.get_or_set().await {
+            ValueOrSetter::Value(value) => value,
+            ValueOrSetter::Setter(setter) => setter.set(5),
+        };
+        assert_eq!(*value, 5)
+    }
+
+    #[tokio::test]
+    async fn get_and_set_double() {
+        let lazy: Lazy<i32> = Lazy::new();
+        match lazy.get_or_set().await {
+            ValueOrSetter::Value(_) => unreachable!(),
+            ValueOrSetter::Setter(setter) => setter.set(5),
+        };
+        match lazy.get_or_set().await {
+            ValueOrSetter::Value(_) => (),
+            ValueOrSetter::Setter(_) => unreachable!(),
+        };
     }
 }
