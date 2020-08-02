a6718dbc606ef5b3b367b57c574d7d6957a5c734
Daniel <codinaut@gmail.com>
2020-08-02 09:20:47 +0700

    fix: crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,37 @@
+#![warn(missing_docs)]
+//! Initialize variables lazily.
+//! Instead of requiring a closure to initialize the value, caller can acts based on returned `ValueOrSetter`.
+//!
+//! # Example
+//! ```
+//! # tokio::runtime::Runtime::new().unwrap().block_on (async {
+//! use lazy_futuristic::{Lazy, ValueOrSetter};
+//!
+//! let lazy_number: Lazy<i32> = Lazy::new();
+//! let number = match lazy_number.get_or_set().await {
+//!     ValueOrSetter::Value(value) => value,
+//!     ValueOrSetter::Setter(setter) => setter.set(10),
+//! };
+//!
+//! assert_eq!(*number, 10);
+//! # });
+//! ```
+
 use futures::lock::{Mutex, MutexGuard};
 use std::cell::UnsafeCell;
 use std::sync::atomic::{AtomicBool, Ordering};
 
+/// Indicates the value or needs to set the value.
 #[derive(Debug)]
 pub enum ValueOrSetter<'l, T> {
+    /// Initialization has been done and value is returned.
     Value(&'l T),
+
+    /// Initialization hasn't been done yet and caller has chance to initialize the value.
     Setter(Setter<'l, T>),
 }
 
+/// Setter of Lazy objects which also acts as Mutex guard.
 #[derive(Debug)]
 pub struct Setter<'l, T> {
     parent: &'l Lazy<T>,
@@ -15,6 +39,9 @@ pub struct Setter<'l, T> {
 }
 
 impl<'l, T> Setter<'l, T> {
+    /// Set the value.
+    ///
+    /// Returns a reference of the stored value.
     pub fn set(&self, value: T) -> &'l T {
         unsafe {
             *self.parent.value.get() = Some(value);
@@ -24,6 +51,7 @@ impl<'l, T> Setter<'l, T> {
     }
 }
 
+/// Lazy variable.
 #[derive(Debug)]
 pub struct Lazy<T> {
     is_initialized: AtomicBool,
@@ -40,6 +68,7 @@ impl<T> Default for Lazy<T> {
 }
 
 impl<T> Lazy<T> {
+    /// Create a new lazy variable.
     pub fn new() -> Self {
         Self {
             is_initialized: AtomicBool::new(false),
@@ -56,6 +85,7 @@ impl<T> Lazy<T> {
         }
     }
 
+    /// Get the value or setter will be returned if value is not available.
     #[allow(clippy::needless_lifetimes)]
     pub async fn get_or_set<'l>(&'l self) -> ValueOrSetter<'l, T> {
         if let Some(value) = self.get() {
@@ -72,6 +102,7 @@ impl<T> Lazy<T> {
         })
     }
 
+    /// Get the value if available.
     pub fn get(&self) -> Option<&T> {
         if self.is_initialized.load(Ordering::Acquire) {
             Some(self.extract())
